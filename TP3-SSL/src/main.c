93e868f0fb32d0f7f202ffbfa87c40957b7aa66a
Unknown <shevchuk.miguel@gmail.com>
2018-09-24 00:43:53 -0300

    Agrego un poco de manejo de errores

--- TP3-SSL/src/main.c
+++ TP3-SSL/src/main.c
@@ -10,8 +10,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "tokens.h"
+#include "scanner.h"
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+char *token_names[] = {"ERROR_CONSTANTE", "ERROR_COMUN", "ERROR_IDENTIFICADOR", "PROGRAMA", "FIN", "VARIABLES", "CODIGO", "DEFINIR", "LEER", "ESCRIBIR", "CONSTANTE", "IDENTIFICADOR", "OPERADOR", "CARACTER_PUNTUACION", "ASIGNACION"};
+int main() {
+	enum token t;
+	while ((t = yylex()) != EOF)
+		printf("Token: %s\t\tValor: %s\n", token_names[t], yytext);
+	return 0;
 }
