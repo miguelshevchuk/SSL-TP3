2a59ab991d354afb8c57ed300747105e6e88e6bb
Unknown <shevchuk.miguel@gmail.com>
2018-10-08 09:35:26 -0300

    Correcciones TP3

--- TP3-SSL/src/main.c
+++ TP3-SSL/src/main.c
@@ -13,15 +13,22 @@
 #include "tokens.h"
 #include "scanner.h"
 
-char *token_names[] = {"Fin de Archivo","Programa", "Fin", "Variables", "Codigo", "Definir", "Leer", "Escribir", "CONSTANTE", "IDENTIFICADOR", "OPERADOR", "CARACTER_PUNTUACION", "ASIGNACION"};
+char *token_names[] = {"Fin de Archivo","Programa", "Fin", "Variables", "Codigo", "Definir", "Leer", "Escribir", "Constante", "Identificador", "Asignacion", "Suma", "Resta", "Multiplicacion", "Division", "Parentesis_abre", "Parentesis_cierra", "Punto", "Coma"};
 int main() {
 	enum token t;
 	while (t = yylex()){
 		if(t == CONSTANTE || t == IDENTIFICADOR){
 			printf("Token: %s\t\tLexema: %s\n", token_names[t], yytext);
+		}else if(esCaracter(t)){
+			printf("Token: '%c'\n", yytext[0]);
 		}else{
-			printf("Token: '%s'\n", yytext);
+			printf("Token: %s\n", token_names[t]);
 		}
+
 	}
 	return 0;
+}
+
+int esCaracter(token){
+	return token == SUMA || token == RESTA || token == MULTIPLICACION || token == DIVISION || token == PARENTESISABRE || token == PARENTESISCIERRA || token == PUNTO || token == COMA;
 }
